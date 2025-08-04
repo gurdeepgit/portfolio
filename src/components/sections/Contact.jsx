218c84c7f74ca0e6c98fde2f38616bfb914da09c
Gurdeep Singh <gurdeep.eec70@gmail.com>
2025-08-04 14:51:15 -0600

    fixes for contact from mobile screen

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -27,7 +27,7 @@ export const Contact = () => {
     return(
         <section id="contact" className="min-h-screen flex items-center justify-center py-20">
         <RevealOnScroll>
-            <div className="px-4 w-150">
+            <div className="w-80 sm:w-96 md:w-120 lg:w-150 mx-auto">
             <h2 className="font-bold text-3xl mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">Get In Touch</h2>
             <form className="space-y-6" onSubmit={handleSubmit}>
                 <div className="relative">
